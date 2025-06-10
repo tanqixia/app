78523edbfbe41bd8c3f5f1cab8bc5fbaa400c7ec
yangyanxi <qq125137@gmail.com>
2025-06-11 01:47:58 +0800

    恢复提交

--- main.py
+++ main.py
@@ -4,8 +4,3 @@ print("hello world")
 # 这是一次测试提交
 if __name__ == '__main__':
     print("hello world")
-    print("hello world")
-    print("hello world")
-    print("hello world")
-    # 这是最新一次的提交
-    # 看是否嗯呢正常提交修改
