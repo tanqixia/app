6d2d75f2f0a1de499266e14500e103aa31cfefc4
yangyanxi <qq125137@gmail.com>
2025-06-12 02:52:36 +0800

    测试同步

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 # 这是一个测试项目
-print("hello world")
-print("hello world")
 # 这是一次测试提交
 if __name__ == '__main__':
     print("hello world")
-    print("hello world")
-    print("hello world")
-    print("hello world")
 
-
-    print("hello world")
-
-    print("hello world")
